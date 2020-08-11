0c43dfef46f85fe2f22d22308c2d79d76f829703
Retno Ayu Adiati <retno_ayu@hotmail.com>
2020-08-11 12:16:06 +0200

    Add components folder
    Clean up unnecessary files
    
    Install gh-pages dependency

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import Button from './components/Button'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [count, setCount] = useState(0)
+  
+  const incrementCount = (increment) => {
+    setCount(count + increment)
+  }
+
+	return (
+		<div className='App'>
+			<Button increment={1} onClickFunction={incrementCount} />
+			<Button increment={10} onClickFunction={incrementCount} />
+			<Button increment={100} onClickFunction={incrementCount} />
+			<Button increment={1000} onClickFunction={incrementCount} />
+			<span>{count}</span>
+		</div>
+	)
 }
 
-export default App;
+export default App
