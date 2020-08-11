17231d63ff5fa18ce8353937a61c23ce6b434416
Retno Ayu Adiati <retno_ayu@hotmail.com>
2020-08-11 12:19:01 +0200

    Updates

---
+++ precache-manifest.9153ee9702c5eaa17997fd40132b440c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0e6ebe9fbffa2f5c3aa8488aae5e42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60a29992b91ae0ea6a5f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b5ae8ded2be0e95f35e8",
+    "url": "/static/js/2.b2f1ccc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b2f1ccc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a29992b91ae0ea6a5f",
+    "url": "/static/js/main.259409c3.chunk.js"
+  },
+  {
+    "revision": "e744f14f3a88f64dcdb4",
+    "url": "/static/js/runtime-main.ea513989.js"
+  }
+]);
